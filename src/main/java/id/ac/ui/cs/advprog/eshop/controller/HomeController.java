111dfd3faa212f4f8cc4c54aa26d9bdce9703046
Nadhif Ibrahim <mnadhifibr@email.com>
2026-02-20 22:21:38 +0700

    fix 1 issue from PMD

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomeController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    public HomeController() {
+
+    }
+
     @GetMapping("/")
     public String homePage() {
         return "homePage";
